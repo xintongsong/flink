01197a7d116f0c9fe9fefae3dc9cf775abcb5811
Xintong Song <tonysong820@gmail.com>
2020-03-08 16:37:48 +0800

    [hotfix][yarn][test] Avoid using Mockito for NMClientAsync in YarnResourceManagerTest.

---
+++ flink-yarn/src/test/java/org/apache/flink/yarn/TestingYarnNMClientAsync.java
@@ -0,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.yarn;
+
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.util.Preconditions;
+
+import org.apache.hadoop.yarn.api.records.Container;
+import org.apache.hadoop.yarn.api.records.ContainerId;
+import org.apache.hadoop.yarn.api.records.ContainerLaunchContext;
+import org.apache.hadoop.yarn.api.records.NodeId;
+import org.apache.hadoop.yarn.client.api.async.NMClientAsync;
+import org.apache.hadoop.yarn.client.api.async.impl.NMClientAsyncImpl;
+
+import java.util.function.Consumer;
+
+/**
+ * A Yarn {@link NMClientAsync} implementation for testing.
+ */
+class TestingYarnNMClientAsync extends NMClientAsyncImpl {
+
+	private Consumer<Tuple3<Container, ContainerLaunchContext, CallbackHandler>> startContainerAsyncConsumer = ignored -> {};
+	private Consumer<Tuple3<ContainerId, NodeId, CallbackHandler>> stopContainerAsyncConsumer = ignored -> {};
+
+	TestingYarnNMClientAsync(final CallbackHandler callbackHandler) {
+		super(callbackHandler);
+	}
+
+	@Override
+	public void startContainerAsync(Container container, ContainerLaunchContext containerLaunchContext) {
+		this.startContainerAsyncConsumer.accept(Tuple3.of(container, containerLaunchContext, callbackHandler));
+	}
+
+	@Override
+	public void stopContainerAsync(ContainerId containerId, NodeId nodeId) {
+		this.stopContainerAsyncConsumer.accept(Tuple3.of(containerId, nodeId, callbackHandler));
+	}
+
+	void setStartContainerAsyncConsumer(Consumer<Tuple3<Container, ContainerLaunchContext, CallbackHandler>> startContainerAsyncConsumer) {
+		this.startContainerAsyncConsumer = Preconditions.checkNotNull(startContainerAsyncConsumer);
+	}
+
+	void setStopContainerAsyncConsumer(Consumer<Tuple3<ContainerId, NodeId, CallbackHandler>> stopContainerAsyncConsumer) {
+		this.stopContainerAsyncConsumer = Preconditions.checkNotNull(stopContainerAsyncConsumer);
+	}
+}
